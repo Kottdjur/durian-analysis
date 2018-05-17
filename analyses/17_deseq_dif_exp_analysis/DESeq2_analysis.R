c8b659d7cf1fcfa5eb350e0a04d31ba5f866374e
Emma Östlund <emmaostl@IBG-D0022.ibg.uu.se>
2018-05-17 15:18:47 +0200

    DESeq

--- analyses/17_deseq_dif_exp_analysis/DESeq2_analysis.R
+++ analyses/17_deseq_dif_exp_analysis/DESeq2_analysis.R
@@ -1,10 +1,11 @@
 library("DESeq2")
 library("pheatmap")
+library("RColorBrewer")
 
 # Load dataset
 directory <- "/home/emmaostl/Documents/durian-analysis/analyses/16_htsseq_read_count/"
 sampleFiles <- grep("MK", list.files(directory), value=TRUE)
-sampleCondition <- c("aril","other","other","other")
+sampleCondition <- c("aril","aril","aril","other","other","other")
 sampleTable <- data.frame(sampleName = sampleFiles,
                           fileName = sampleFiles,
                           condition = sampleCondition)
@@ -23,9 +24,27 @@ resOrdered <- res[order(res$pvalue),]
 resOrdered
 summary(res)
 
-plotMA(res, ylim=c(-8,8))
+# Export results to csv
+write.table(as.data.frame(res), file="/home/emmaostl/Documents/durian-analysis/analyses/17_deseq_dif_exp_analysis/DESeq2_results.csv", 
+          sep="\t", quote=FALSE, col.names=NA)
 
-# Heatmap
+# Number of genes with adjusted p-value < 0.05
+sum(res$padj < 0.05, na.rm=TRUE)
+
+# Plot
+plotMA(res, alpha=0.05)
+
+# Heatmaps
 select <- order(rowMeans(counts(dds,normalized=TRUE)), decreasing=TRUE)[1:20]
 df <- as.data.frame(colData(dds)[,c("condition","sizeFactor")])
 pheatmap(assay(dds)[select,],cluster_rows=FALSE, show_rownames=FALSE, cluster_cols=FALSE, annotation_col=df)
+
+sampleDists <- dist(t(assay(dds)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(dds$condition)
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
