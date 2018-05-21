1af5a19d00dcf21dc112f24c8bb8ed66962f4fcf
Emma <kottdjur@hotmail.com>
2018-05-21 11:38:19 +0200

    Results

--- code/python_scripts/expression_annotation.py
+++ code/python_scripts/expression_annotation.py
@@ -1,3 +1,6 @@
+# Combined DESeq2 and eggNOG results into a table with columns for:
+# gene id,  adjusted p-val, COG functional category and annotation.
+
 import csv
 import re
 
@@ -5,15 +8,16 @@ deseq_path = "H:/Dokument/durian-analysis/analyses/17_deseq_dif_exp_analysis/DES
 eggnog_path = "H:/Dokument/durian-analysis/analyses/18_eggnog_functional_annotation/eggnog_maker_protein_annotations.tsv"
 
 # Open DESeq output and save to array.
-with open(deseq_path, "r") as deseq_file:
+with open(deseq_path, "rb") as deseq_file:
     tsvin = csv.reader(deseq_file, delimiter="\t")
     deseq_array = []
 
     tsvin.next()    # Skip column names
 
     for row in tsvin:
+        row[0] = re.sub("^augustus_masked-NW_019168381.1-processed-gene-", "", row[0])
         for i in range(len(row)):
-            if i != 0 and row[i] != "NA":
+            if row[i] != "NA":
                 row[i] = float(row[i])
 
         deseq_array.append(row)
@@ -21,12 +25,14 @@ with open(deseq_path, "r") as deseq_file:
 deseq_file.close()
 
 # Open eggNOG output and save to array.
-with open(eggnog_path, "r") as eggnog_file:
+with open(eggnog_path, "rb") as eggnog_file:
     tsvin = csv.reader(eggnog_file, delimiter="\t")
     eggnog_array = []
 
     for row in tsvin:
         row[0] = re.sub("-mRNA-1$", "", row[0])
+        row[0] = re.sub("^augustus_masked-NW_019168381.1-processed-gene-", "", row[0])
+        row[0] = float(row[0])
         eggnog_array.append(row)
 
 eggnog_file.close()
@@ -34,34 +40,36 @@ eggnog_file.close()
 # Take subset of statistically relevant genes
 deseq_subset = [line for line in deseq_array if line[6] < 0.05]
 
-# Sort the subset and eggnog annotations based on gene names
+# Sort the subset and eggnog annotations based on gene number
 deseq_subset = sorted(deseq_subset, key=lambda x: x[0])
 eggnog_array = sorted(eggnog_array, key=lambda x: x[0])
 
-print "Subset"
-for i in range(20):
-    print deseq_subset[i]
-print "eggNOG"
-for i in range(100):
-    print eggnog_array[i]
-
-print "end"
+# Match annotation to genes
 combined = []
 egg_inx = 0
 
 for line in deseq_subset:
-    deseq_name = line[0]
-    eggnog_name = eggnog_array[egg_inx][0]
-    match = False
-    while not match:
-        if deseq_name == eggnog_name:
-            combined.append([deseq_name, line[6], eggnog_array[egg_inx][11], eggnog_array[egg_inx][12]])
-            print len(combined)
-            egg_inx += 1
-            match = True
-        else:
-            egg_inx += 1
-            eggnog_name = eggnog_array[egg_inx][0]
-            match = False
+    deseq_id = line[0]
+    eggnog_id = eggnog_array[egg_inx][0]
+    while deseq_id > eggnog_id:
+        egg_inx += 1
+        eggnog_id = eggnog_array[egg_inx][0]
 
-print combined
+    if deseq_id == eggnog_id and eggnog_array[egg_inx][12] != '':
+        combined.append([deseq_id, line[6], eggnog_array[egg_inx][11], eggnog_array[egg_inx][12]])
+        egg_inx += 1
+
+
+filter_comb = [line for line in combined if line[2] not in ['A', 'B', 'C', 'J', 'K', 'L', 'M', 'P', 'S', 'Z']]
+filter_comb = sorted(filter_comb, key=lambda x: x[1])
+for line in filter_comb:
+    print line
+
+
+print len(deseq_subset)
+print len(combined)
+print len(filter_comb)
+
+writer = csv.writer(open("H:/Dokument/durian-analysis/analyses/significant_genes.csv", 'wb'))
+for line in combined:
+    writer.writerow(line)
